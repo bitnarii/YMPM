4e4b752cf5788f11d61201d19a17117c98956c31
bitnarii <noreply_@hanmail.net>
2022-03-23 14:42:16 +0900

    back 스프링프로젝트 생성

---
+++ back/ympm/src/main/java/com/fitback/ympm/YmpmApplication.java
@@ -0,0 +1,13 @@
+package com.fitback.ympm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YmpmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YmpmApplication.class, args);
+	}
+
+}
