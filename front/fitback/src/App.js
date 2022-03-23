92072da79605e8ba9da1d4587d122cb7fa3e99a6
Gwak-daeun <bbibibbibi22@gmail.com>
2022-03-23 17:23:58 +0900

    곽다은 index.js, app.js 취합(데일리룩, 댓글, 헤더)

--- front/fitback/src/App.js
+++ front/fitback/src/App.js
@@ -9,6 +9,13 @@ function App() {
                 {/* 효원 (옷장, 옷장 불러오기) */}
                 <Route path="/closet" element={<Closet />} />
                 <Route path="/closetList" element={<ClosetList />} />
+                {/* 곽다은 (데일리룩, 댓글) */}
+                <Route path='/' element={<Home />}/>
+                <Route path='/addDailylook' element={<AddDailylook />} />
+                <Route path='/editDailylook' element={<EditDailylook />} />
+                <Route path='/dailylook' element={<Dailylook />} />
+                <Route path='/op' element={<Opinion />} />
+
             </Routes>
         </BrowserRouter>
     );
