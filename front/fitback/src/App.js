08abde14cf99c994d35bb2f02efcb3092a07fefd
bitnarii <noreply_@hanmail.net>
2022-03-23 17:30:12 +0900

    app.js, index.html - 수정(나리)

--- front/fitback/src/App.js
+++ front/fitback/src/App.js
@@ -1,6 +1,12 @@
 import "./App.css";
+import BookmarkList from "./component/page/BookmarkList";
 import Closet from "./component/page/Closet";
 import ClosetList from "./component/page/ClosetList";
+import Login from "./component/page/Login";
+import Register from "./component/page/Register";
+import Search from "./component/page/Search";
+import SearchResult from "./component/page/SearchResult";
+import UserEdit from "./component/page/UserEdit";
 
 function App() {
     return (
@@ -15,6 +21,13 @@ function App() {
                 <Route path='/editDailylook' element={<EditDailylook />} />
                 <Route path='/dailylook' element={<Dailylook />} />
                 <Route path='/op' element={<Opinion />} />
+                {/* 권빛나리 (회원관리, 북마크, 검색) */}
+                <Route path="/login" element={<Login/>} /> 
+                <Route path="/bookmarklist" element={<BookmarkList/>} /> 
+                <Route path="/register" element={<Register/>} /> 
+                <Route path="/search" element={<Search/>} /> 
+                <Route path="/searchresult" element={<SearchResult/>} /> 
+                <Route path="/useredit" element={<UserEdit/>} /> 
 
             </Routes>
         </BrowserRouter>
