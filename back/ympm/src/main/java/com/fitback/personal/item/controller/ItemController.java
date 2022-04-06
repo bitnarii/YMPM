cf8ebdf501d6392cfa9afc14a2d948ef5fad013c
gxdnxs <projectann215@gmail.com>
2022-04-06 14:43:17 +0900

    Update ItemController.java

--- back/ympm/src/main/java/com/fitback/personal/item/controller/ItemController.java
+++ back/ympm/src/main/java/com/fitback/personal/item/controller/ItemController.java
@@ -54,7 +54,14 @@ public class ItemController {
             // 파일 원본명 가져오기
             String fileName  = file.getOriginalFilename();
             //파일이 업로드 될 경로 설정
-            File fileAddr = new File("C:/Users/admin/Documents/FinalProject/myFitBack/imgFile/" + fileName);
+            File fileAddr = new File("C:/Users/admin/Documents/myFitBack_imgFile/" + fileName);
+            //경로 없을 시 생성 (상대 경로 생성 방법 모르겠어서 일단 이렇게 유지 ㅠㅠ)
+            if ( ! fileAddr.exists()) {
+                // (밑에 건 경고무시문?) mkdir 에 경고문 떠서 아래 문구 추가하니 사라짐
+                //noinspection ResultOfMethodCallIgnored
+                new File("C:/Users/admin/Documents/myFitBack_imgFile").mkdir();
+            }
+
             //파일 저장
             file.transferTo(fileAddr);
         }
